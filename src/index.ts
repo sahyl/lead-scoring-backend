9d20f8c8f243d739a2deec6c1618013d614bfaa3
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2025-09-25 14:55:13 +0530

    Implement in-memory storage for offer, leads, and scored results and updated files

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import GEMINI_API_KEY from
---
+++ src/storage.ts
@@ -0,0 +1,5 @@
+import {Offer , Lead , ScoreLead} from "./types"
+
+export let currentOffer:Offer|null = null
+export let leads :Lead[]=[]
+export let scoredLead :ScoreLead[]=[]
