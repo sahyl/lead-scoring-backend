5d9c8fff6907450c46786cff2528ca2203ae0de7
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2025-09-26 03:33:22 +0530

    Set up main Express server with route integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-import GEMINI_API_KEY from
+import express from "express"
+import router from "./routes.js"
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(router)
+
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
